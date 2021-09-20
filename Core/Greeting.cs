e1cacbc798294214ccad6ce6870543979fe43f6b
Sergio Bermudez <sbermudez@rsimail.com>
2021-09-19 23:05:11 -0500

    initial repository

---
+++ Core/Greeting.cs
@@ -0,0 +1,9 @@
+﻿
+namespace Core;
+public class Greeting
+{
+    public string SayHello(string name)
+    {
+        return $"Hi {name}!!!";
+    }
+}
---
+++ DeskApp/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Core;
+
+Greeting greeting = new Greeting();
+
+Console.WriteLine(greeting.SayHello("Sergio"));
+Console.ReadKey();
